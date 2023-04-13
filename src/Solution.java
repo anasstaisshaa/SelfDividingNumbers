7fe5af5e869f4360e27c2da32e091ca5aa596d2b
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-04-13 12:48:47 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+
+        for(int i = left; i<= right; i++){
+            if(isDividing(i))
+                ans.add(i);
+        }
+        return ans;
+    }
+
+    public boolean isDividing (int num){
+        for (char c: String.valueOf(num).toCharArray()) {
+            if (c == '0' || (num % (c - '0') > 0))
+                return false;
+        }
+        return true;
+    }
+}
